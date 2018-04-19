4c0df68a99279fe858d8cfb5bb9f6ce0c41cce45
sutao <sutaoem@163.com>
2018-04-19 22:17:14 +0800

    提交pom.xml

---
+++ src/main/java/com/sutao/entity/UserEntity.java
@@ -0,0 +1,13 @@
+package com.sutao.entity;
+
+/**
+ * Created by sue on 2018/4/19.
+ */
+public class UserEntity {
+
+    private String userId;
+
+    private String userName;
+
+    private String password;
+}
